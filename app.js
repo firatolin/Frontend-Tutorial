0ff7afbf4e83c8f099afe0ccd13dc33e9b5459c6
Firatol Esayas <teferafiratolesayas@gmail.com>
2025-07-22 14:14:09 +0300

    Js Added

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("Hello, World!...How are you?");
+console.log("This is a simple Node.js application.");
+console.log("It prints messages to the console.");
+console.log("You can run this application using Node.js.");
+console.log("Make sure you have Node.js installed on your machine.");
