845fc52de3f74f9e63a7433f675e195e86062ec4
Firatol Esayas <teferafiratolesayas@gmail.com>
2025-07-30 19:19:17 +0300

    Febonacci sequence

--- app.js
+++ app.js
@@ -244,3 +244,14 @@ function factorial(n) {
 console.log(
   "This code demonstrates a simple for loop that prints the factorial of numbers from 1 to 10."
 );
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`Fibonacci of ${i} is ${fibonacci(i)}`);
+}
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+console.log(
+  "This code demonstrates a simple for loop that prints the Fibonacci sequence up to 10."
+);
